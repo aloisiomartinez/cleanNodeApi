d4b483a223d51f8489becead55f33542df78b8a8
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-07-12 08:22:04 -0300

    test: ensure DBAuthentication throws if TokenGenerator throwss

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -20,10 +20,12 @@ export class DbAuthentication implements Authentication {
 
   async auth (authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
+
     if (account) {
       await this.hashComparer.compare(authentication.password, account.password)
       await this.tokenGenerator.generate(account.id)
     }
+
     return null
   }
 }
