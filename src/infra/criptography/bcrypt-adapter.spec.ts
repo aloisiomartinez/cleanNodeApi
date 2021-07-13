e06651307c00ac271366c485f080f2c6baebd387
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-07-13 08:34:00 -0300

    feat: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -40,7 +40,13 @@ describe('Bcrypt Adapter', () => {
   test('Should call compare with correct values', async () => {
     const sut = makeSut()
     const compareSpy = jest.spyOn(bcrypt, 'compare')
-    sut.compare('any_value', 'any_hash')
-    await expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+    await sut.compare('any_value', 'any_hash')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+  })
+
+  test('Should call true when compare succeeds', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
   })
 })
