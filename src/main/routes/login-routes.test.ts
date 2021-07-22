716509ef35fb9fa63785f7f6b8032e847488cf12
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-07-22 15:23:33 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,19 +16,38 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
   describe('POST /signup Routes', () => {
     test('Should return 200 on signup', async () => {
       await request(app)
-        .get('/api/signup')
+        .post('/api/signup')
         .send({
           name: 'Aloisio Martinez',
           email: 'aloisio.jacobs@hotmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login Routes', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123',12)
+
+      await accountCollection.insertOne({
+        name: 'Aloisio Martinez',
+        email: 'aloisio.jacobs@hotmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'aloisio.jacobs@hotmail.com',
+          password: '123'
         })
         .expect(200)
     })
