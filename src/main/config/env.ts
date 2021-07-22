97dd15c78fadffa5cbb38e5e1e60e25f919d8b63
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-07-22 16:15:03 -0300

    refactor: change routes to use filesystem instead of fast-glob

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://aloisiom:d875efef@cluster0.3tzxi.mongodb.net/test',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==2'
 
