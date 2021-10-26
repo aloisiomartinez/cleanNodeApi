4978da7f1a268e2ef08798594cb14f707c528712
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-10-26 18:32:12 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('CORS Middleware', () => {
-  test('should penable CORS', async () => {
+  test('should eenable CORS', async () => {
     app.get('/test_cors', (req, res) => {
       res.send()
     })
